3c987c0e53a5ce707749bf2b2608e5776c641e6e
sowgandhi11 <sowgandhi.bhattu11@gmail.com>
2021-05-06 18:17:06 -0400

    fixing paragraph on about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,7 +14,7 @@ export class About extends React.Component {
             <div className='col-xs-12 col-md-6'>
               <div className='about-text'>
                 <h2>About The Baker</h2>
-                <p>"TT’s Creative Confections is an in home bakery located in Palatine, IL. Tianna Turton is the self taught baker/decorator. Tianna started TT’s Creative Confections back in 2013 while she was in college at ISU. TT’s Creative Confections offers beautiful and creative customized cake, cupcakes, pie and pastry orders."</p>
+                <p>TT’s Creative Confections is an in home bakery located in Palatine, IL. Tianna Turton is the self taught baker/decorator. Tianna started TT’s Creative Confections back in 2013 while she was in college at ISU. TT’s Creative Confections offers beautiful and creative customized cake, cupcakes, pie and pastry orders.</p>
 
                 <div className='list-style'>
                   <h3>Follow us on Instagram</h3>
